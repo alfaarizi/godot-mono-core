6ba58df5add8fb29788b8f52f6154e2c0b748758
Muhammad Al Farizi <ziziyusrizal@gmail.com>
2025-06-15 18:23:56 +0200

    chore: add sample unit test file

--- tests/PlayerTest.cs
+++ tests/PlayerTest.cs
@@ -4,16 +4,10 @@ using static GdUnit4.Assertions;
 [TestSuite]
 public class PlayerTest
 {
-    // [TestCase]
-    // public static void TestPlayerCreation()
-    // {
-    //     var player = new Player();
-    //     AssertThat(player).IsNotNull();
-    // }
-
-    // [TestCase]
-    // public static void TestPlayerSpeed()
-    // {
-    //     _ = AssertThat(Player.Speed).IsEqual(500.0f);
-    // }
+    [TestCase]
+    public void TestPlayerCreation()
+    {
+        var player = new Player();
+        AssertThat(player).IsNotNull();
+    }
 }
